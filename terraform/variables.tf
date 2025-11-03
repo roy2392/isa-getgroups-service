f080915231c8a805bbf9ca22d4af4371b38b8cdf
Roy Zalta <roy@example.com>
2025-11-03 19:19:56 +0200

    fix: Declare terraform variables and remove tfvars

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "project_id" {
+  description = "The GCP project ID."
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region to deploy resources in."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "service_name" {
+  description = "The name of the Cloud Run service."
+  type        = string
+  default     = "telegram-groups-classifier"
+}
+
+variable "image_url" {
+  description = "The URL of the Docker image to deploy."
+  type        = string
+}
